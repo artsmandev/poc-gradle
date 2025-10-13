3ecfec73eb41df15229249d389e5e4a32224060a
Bruno Andrade <bruno@artsman.dev>
2025-10-13 18:38:26 +0100

    (feat): lombok + mapstruct.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,36 @@
+import org.gradle.kotlin.dsl.annotationProcessor
+
 group = "dev.artsman.poc"
 version = "0.0.1-SNAPSHOT"
 description = "PoC Gradle Project"
 
 plugins {
 	java
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	annotationProcessor("org.projectlombok:lombok:1.18.42")
+	annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
+
+	compileOnly("org.projectlombok:lombok:1.18.42")
+	implementation("org.mapstruct:mapstruct:1.6.3")
+
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher:6.0.0")
+	testCompileOnly("org.junit.jupiter:junit-jupiter:6.0.0")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter:6.0.0")
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+tasks.test {
+	useJUnitPlatform()
 }
